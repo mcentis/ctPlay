f17e2cafcb6ab3603a07b0d3aa0dfe8d3a25d4c7
mcentis-uhh <matteo.centis.vignali@desy.de>
2016-03-21 15:54:16 +0100

    added function to plot stuff, modified phantom, added plots for new functionalities

--- test.py
+++ test.py
@@ -6,7 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import *
 
-lis = [circle(np.array([1, 5]), 2, 0.5), circle(np.array([-1, -3]), 1, 0.2)]
+def showStuff(inp):
+    h = inp[0]
+    yEdges = inp[1]
+    xEdges = inp[2]
+    im = plt.imshow(h, interpolation='none', origin='low', extent=[xEdges[0], xEdges[-1], yEdges[0], yEdges[-1]], aspect='auto')#, cmap='Greys_r')
+    plt.colorbar(im)
+
+lis = [circle(np.array([1, 5]), 2, 0.5), circle(np.array([-1, -3]), 1, 0.2), circle(np.array([-3, 0]), 0.1, 5)]
 #lis = [circle(np.array([-1, -3]), 1, 2)]
 pa = phantom(lis)
 
@@ -16,25 +23,33 @@ plt.ylim(-15, 15)
 pa.draw(plt)
 
 ct = ctScan(pa, radians(0), radians(180), radians(1), -10, 10, 0.15, 15)
-ct.scan()
-h = ct.sinogram[0]
-yEdges = ct.sinogram[1]
-xEdges = ct.sinogram[2]
 
+ct.scan()
 fig = plt.figure(2)
-im = plt.imshow(h, interpolation='none', origin='low', extent=[xEdges[0], xEdges[-1], yEdges[0], yEdges[-1]], aspect='auto')
-plt.colorbar(im)
+showStuff(ct.sinogram)
 
 rec = backProjection(ct.sinogram, 15, 0.15)
-rec.reconstruct()
 
-h = rec.image[0]
-yEdges = rec.image[1]
-xEdges = rec.image[2]
-
+rec.oneMinusSino()
 fig = plt.figure(3)
-im = plt.imshow(h, interpolation='none', origin='low', extent=[xEdges[0], xEdges[-1], yEdges[0], yEdges[-1]], aspect='auto')
-plt.colorbar(im)
+showStuff(rec.sinogram)
 
+rec.reconstruct()
+fig = plt.figure(4)
+showStuff(rec.image)
+# h = rec.sinogram[0]
+# ed = rec.sinogram[2]
+# plt.plot(ed[:-1], h[0])
+
+rec.filterSino()
+fig = plt.figure(5)
+showStuff(rec.sinogram)
+# h = rec.sinogram[0]
+# ed = rec.sinogram[2]
+# plt.plot(ed[:-1], h[0])
+
+rec.reconstruct()
+fig = plt.figure(6)
+showStuff(rec.image)
 
 plt.show()
